97224f5013f89090d6bf718fabe803a79faf9987
bruno-devlearn <qbruno570@gmail.com>
2025-08-16 17:46:45 -0300

    clock completo

---
+++ aula004/exercicio/clocker/src/components/main/Main.jsx
@@ -0,0 +1,10 @@
+import Clock from "./components/clock";
+import "../css/main.css";
+
+export default function Main() {
+    return (
+        <main>
+            <Clock />
+        </main>
+    );
+}
---
+++ aula004/exercicio/clocker/src/components/main/components/clock.jsx
@@ -0,0 +1,27 @@
+export default function Clock() {
+    return (
+        <div className="content">
+            <div className="clock">
+                <div className="date">
+                    <span id="week-day">Saturday, </span>
+                    <span id="mouth">August </span>
+                    <span id="day">16, </span>
+                    <sapn id="year">2025</sapn>
+                </div>
+                <div className="time">
+                    <span id="hour">16:</span>
+                    <span id="minute">30:</span>
+                    <span id="seconds">30</span>
+                </div>
+                <div className="buttons">
+                    <button id="12h" className="no-selected">
+                        12h
+                    </button>
+                    <button id="24h" className="selected">
+                        24h
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
