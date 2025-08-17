0415d6f25b862ba0e4ea9fdc869862f99f548969
bruno-devlearn <qbruno570@gmail.com>
2025-08-16 23:09:51 -0300

    switcher por obj

--- aula004/exercicio/clocker/src/components/header/Header.jsx
+++ aula004/exercicio/clocker/src/components/header/Header.jsx
@@ -1,16 +1,30 @@
 import "../css/header.css";
 
-export default function Header() {
+export default function Header({ option, setOption }) {
     return (
         <header>
             <div className="switcher">
-                <span className="switch" id="Clock">
+                <span
+                    className={`switch ${option === "clock" ? "active" : ""}`}
+                    spans:id="Clock"
+                    onClick={() => setOption("clock")}
+                >
                     Clock
                 </span>
-                <span className="switch" id="Stopwatch">
+                <span
+                    className={`switch ${
+                        option === "stopwatch" ? "active" : ""
+                    }`}
+                    id="Stopwatch"
+                    onClick={() => setOption("stopwatch")}
+                >
                     Stopwatch
                 </span>
-                <span className="switch" id="Timer">
+                <span
+                    className={`switch ${option === "timer" ? "active" : ""}`}
+                    id="Timer"
+                    onClick={() => setOption("timer")}
+                >
                     Timer
                 </span>
             </div>
