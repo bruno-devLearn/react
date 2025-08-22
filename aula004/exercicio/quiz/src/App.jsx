ea9532e538f29c12dc45614401731f4725b94720
bruno-devlearn <qbruno570@gmail.com>
2025-08-22 10:18:48 -0300

    quiz design

--- aula004/exercicio/quiz/src/App.jsx
+++ aula004/exercicio/quiz/src/App.jsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header/Header";
+import { Quiz } from "./components/Main/Quiz";
 import "./App.css";
 
 export default function App() {
     return (
         <>
             <Header />
+            <Quiz />
         </>
     );
 }
