5dba90bfc112abf50c096a072e6d0edf8eb23e9a
bruno-devlearn <qbruno570@gmail.com>
2025-08-23 13:21:27 -0300

    performance display design

--- aula004/exercicio/quiz/src/App.jsx
+++ aula004/exercicio/quiz/src/App.jsx
@@ -1,12 +1,13 @@
 import { Header } from "./components/Header/Header";
 import { Quiz } from "./components/Main/Quiz";
 import "./App.css";
+import { Result } from "./components/Result/Result";
 
 export default function App() {
     return (
         <>
             <Header />
-            <Quiz />
+            <Result />
         </>
     );
 }
