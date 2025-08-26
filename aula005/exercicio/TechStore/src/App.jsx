0d2c3d926a49ca0d474a226633456f0a3ffa452c
bruno-devlearn <qbruno570@gmail.com>
2025-08-26 12:27:26 -0300

    filter bar design

--- aula005/exercicio/TechStore/src/App.jsx
+++ aula005/exercicio/TechStore/src/App.jsx
@@ -1,9 +1,13 @@
 import { Header } from "./components/Header/Header";
+import { Store } from "./components/Store/Store";
 
 function App() {
     return (
         <>
             <Header />
+            <main>
+                <Store />
+            </main>
         </>
     );
 }
