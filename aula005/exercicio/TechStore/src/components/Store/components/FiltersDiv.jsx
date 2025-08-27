607d03c1a48ee62f53e11ceeac10d90b9d6be33a
bruno-devlearn <qbruno570@gmail.com>
2025-08-27 08:29:57 -0300

    filter by price div design

--- aula005/exercicio/TechStore/src/components/Store/components/FiltersDiv.jsx
+++ aula005/exercicio/TechStore/src/components/Store/components/FiltersDiv.jsx
@@ -1,27 +1,12 @@
-import "./css/filtersDiv.css";
-
-/* 
-<div className="select">
-    <label className="checkbox">
-        <input type="checkbox" id="" />
-        <span className="material-symbols-outlined">
-            check
-        </span>
-        <span className="text">beauty</span>
-    </label>
-</div>
-*/
+import { FilterByCategory } from "./FiltersDiv/FilterByCategory";
+import { FilterByPrice } from "./FiltersDiv/FilterByPrice";
+import "./FiltersDiv/css/filtersDiv.css";
 
 export function FiltersDiv() {
     return (
         <>
-            <div className="categorys">
-                <div className="title">
-                    <h2>Categories</h2>
-                    <span className="clear">Clear</span>
-                </div>
-                <div className="scroll"></div>
-            </div>
+            <FilterByCategory />
+            <FilterByPrice />
         </>
     );
 }
---
+++ aula005/exercicio/TechStore/src/components/Store/components/FiltersDiv/FilterByCategory.jsx
@@ -0,0 +1,23 @@
+/* 
+<div className="select">
+    <label className="checkbox">
+        <input type="checkbox" id="" />
+        <span className="material-symbols-outlined">
+            check
+        </span>
+        <span className="text">beauty</span>
+    </label>
+</div>
+*/
+
+export function FilterByCategory() {
+    return (
+        <div className="categories">
+            <div className="title">
+                <h2>Categories</h2>
+                <span className="clear">Clear</span>
+            </div>
+            <div className="scroll"></div>
+        </div>
+    );
+}
