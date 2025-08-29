c5051a15a618c448225f4a26f9fa4d3635207afe
bruno-devlearn <qbruno570@gmail.com>
2025-08-29 16:14:37 -0300

    responsive cards design

--- aula005/exercicio/TechStore/src/App.jsx
+++ aula005/exercicio/TechStore/src/App.jsx
@@ -1,5 +1,6 @@
 import { Cart } from "./components/cart/Cart";
 import { Header } from "./components/Header/Header";
+import { Cards } from "./components/Main/Cards/Card";
 import { Pages } from "./components/Main/Pages";
 import { Store } from "./components/Main/Store";
 
@@ -10,6 +11,7 @@ function App() {
             <main>
                 <Store />
                 <Cart />
+                <Cards />
                 <Pages />
             </main>
         </>
---
+++ aula005/exercicio/TechStore/src/components/Main/Cards/Card.jsx
@@ -0,0 +1,50 @@
+import "./cards.css";
+
+/* 
+<div className="card">
+    <div className="img">
+        <img src="#" />
+        <span className="discount">-10%</span>
+    </div>
+    <div className="information">
+        <div className="category">
+            <span className="text">beauty</span>
+        </div>
+        <div className="info">
+            <h2>Essence Mascara Lash</h2>
+            <div className="quality">
+                <span class="material-symbols-outlined">
+                    star
+                </span>
+                <span className="text">2.6</span>
+                <span className="available">
+                    (99 available)
+                </span>
+            </div>
+            <div className="price">
+                <span className="current">$8.94</span>
+                <span className="old">
+                    <del>$9.99</del>
+                </span>
+            </div>
+            <button className="add-cart">
+                <span class="material-symbols-outlined">
+                    shopping_cart
+                </span>
+                <span className="text">Add</span>
+            </button>
+        </div>
+    </div>
+</div>
+*/
+
+export function Cards() {
+    return (
+        <div className="cards-div">
+            <div className="quant">
+                <span className="text">178 products found (page 1 of 6)</span>
+            </div>
+            <div className="cards"></div>
+        </div>
+    );
+}
