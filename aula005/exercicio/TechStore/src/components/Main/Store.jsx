f340f9444c499936759949b773cc3ef23b07264e
Bruno Queiroz <145293563+bruno-devLearn@users.noreply.github.com>
2025-09-08 22:25:08 -0300

    Merge pull request #42 from bruno-devLearn:actions
    ação de abrir r fechar divs de filtragem

--- aula005/exercicio/TechStore/src/components/Main/Store.jsx
+++ aula005/exercicio/TechStore/src/components/Main/Store.jsx
@@ -1,11 +1,31 @@
+import { useEffect, useState } from "react";
 import "../../css/index.css";
+import { Cart } from "../cart/Cart";
+import { Cards } from "./Cards/Card";
 
 import { FilterBar } from "./components/FilterBar";
+import { FiltersDiv } from "./components/FiltersDiv";
+import { Pages } from "./Pages";
 
 export function Store() {
+    const [open, setOpen] = useState("");
+
+    useEffect(() => {
+        const eventClick = () => setOpen("");
+        document.addEventListener("click", eventClick);
+
+        return () => {
+            document.removeEventListener("click", eventClick);
+        };
+    }, []);
+
     return (
         <>
-            <FilterBar />
+            <FilterBar setOpen={setOpen} />
+            <FiltersDiv open={open} />
+            <Cart />
+            <Cards />
+            <Pages />
         </>
     );
 }
