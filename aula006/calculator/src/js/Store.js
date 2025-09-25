6061332980e71364e46d6abc9de1d2b5b6297e10
bruno-devlearn <qbruno570@gmail.com>
2025-09-25 19:58:02 -0300

    formatação do resultado

--- aula006/calculator/src/js/Store.js
+++ aula006/calculator/src/js/Store.js
@@ -2,12 +2,28 @@ import { create } from "zustand";
 
 export const useStore = create((set) => ({
     inputValue: "",
-    changeInputValue: (value) => set({ inputValue: String(value) }),
+    changeInputValue: (value) => formated(value, set),
     calculateValue: (value) => calculate(value),
 }));
 
+function formated(value, set) {
+    if (
+        value === "número inválido" ||
+        value === "número infinito" ||
+        value === "número muito grande"
+    ) {
+        set({ inputValue: "" });
+    } else if (String(value).length < 47) {
+        set({ inputValue: String(value) });
+    }
+}
+
 function calculate(value) {
-    const sanitized = value.replace(/,/g, ".").replace(/\b0+(\d)/g, "$1");
+    const sanitized = value
+        .replace(/(\d)\.(?=(\d{3})+(\D|$))/g, "$1") // remove pontos que são separadores de milhar
+        .replace(/,/g, ".") // vírgula → ponto decimal
+        .replace(/\b0+(\d)/g, "$1"); // remove zeros à esquerda
+
     let result;
 
     try {
@@ -28,5 +44,5 @@ function calculate(value) {
         return "número muito grande";
     }
 
-    return result;
+    return result.toLocaleString("pt-BR");
 }
