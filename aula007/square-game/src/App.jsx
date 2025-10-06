4b7b03ddf7708054b388aa92f39b726b38471e3b
bruno-devlearn <qbruno570@gmail.com>
2025-10-06 11:24:48 -0300

    tela de inicio

--- aula007/square-game/src/App.jsx
+++ aula007/square-game/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import { Start } from "./components/game-components/Start";
+import "./app.css";
+
+function App() {
+    return (
+        <main>
+            <Start />
+        </main>
+    );
+}
 
 export default App;
