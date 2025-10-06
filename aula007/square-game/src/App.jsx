69ea23d6413e3ed8dc353a7c95d4d3ff3e87ac0e
bruno-devlearn <qbruno570@gmail.com>
2025-10-06 12:07:53 -0300

    design do jogo + fim de jogo

--- aula007/square-game/src/App.jsx
+++ aula007/square-game/src/App.jsx
@@ -1,10 +1,12 @@
 import { Start } from "./components/game-components/Start";
+import { Game } from "./components/game-components/Game";
+import { End } from "./components/game-components/End";
 import "./app.css";
 
 function App() {
     return (
         <main>
-            <Start />
+            <Game />
         </main>
     );
 }
--- aula007/square-game/src/components/game-components/Game.jsx
+++ aula007/square-game/src/components/game-components/Game.jsx
@@ -1,1 +1,29 @@
-export function Game() {}
+import { Grid, GridItem, Box } from "@chakra-ui/react";
+
+export function Game() {
+    return (
+        <div className="game-div">
+            <div className="points">
+                <span className="game-points">Tentativas: 0</span>
+            </div>
+
+            <Grid
+                templateColumns="repeat(5, 1fr)"
+                templateRows="repeat(5, 1fr)"
+                gap="10px"
+            >
+                {Array.from({ length: 25 }).map((_, index) => (
+                    <GridItem key={index}>
+                        <Box
+                            background="yellow.300"
+                            w="150px"
+                            h="150px"
+                            shadow="3px 3px 2px 1px rgba(0, 0, 0, 0.1)"
+                            cursor="pointer"
+                        ></Box>
+                    </GridItem>
+                ))}
+            </Grid>
+        </div>
+    );
+}
