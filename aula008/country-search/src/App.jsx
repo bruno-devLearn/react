a3fa3e1852a23d21405a7ee652e3756f61ee5d8d
bruno-devlearn <qbruno570@gmail.com>
2025-10-15 17:54:52 -0300

    pagination design

--- aula008/country-search/src/App.jsx
+++ aula008/country-search/src/App.jsx
@@ -1,6 +1,7 @@
 import { Header } from "./components/Header";
 import "./app.css";
 import { Cards } from "./components/Cards";
+import { Pages } from "./components/Pages";
 
 function App() {
     return (
@@ -8,6 +9,7 @@ function App() {
             <Header />
             <main>
                 <Cards />
+                <Pages />
             </main>
         </>
     );
