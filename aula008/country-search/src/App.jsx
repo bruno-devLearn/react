ff1cd89cda75b883fc1ed51e80489bba34750fa8
bruno-devlearn <qbruno570@gmail.com>
2025-10-14 23:15:01 -0300

    header design

--- aula008/country-search/src/App.jsx
+++ aula008/country-search/src/App.jsx
@@ -1,3 +1,12 @@
-function App() {}
+import { Header } from "./components/Header/Header";
+import "./app.css";
+
+function App() {
+    return (
+        <>
+            <Header />
+        </>
+    );
+}
 
 export default App;
---
+++ aula008/country-search/src/components/Header/Header.jsx
@@ -0,0 +1,28 @@
+import { GoGlobe } from "react-icons/go";
+import { CiSearch } from "react-icons/ci";
+
+export function Header() {
+    return (
+        <header>
+            <div className="header">
+                <div className="icon">
+                    <GoGlobe />
+                </div>
+                <div className="title">
+                    <h1>Buscador de Países</h1>
+                    <p>Explore informações sobre países ao redor do mundo</p>
+                </div>
+            </div>
+            <div className="input">
+                <div className="icon">
+                    <CiSearch />
+                </div>
+                <input type="text" />
+            </div>
+            <div className="status-div">
+                <span className="countries">30 países encontrados</span>
+                <button>Limpar busca</button>
+            </div>
+        </header>
+    );
+}
